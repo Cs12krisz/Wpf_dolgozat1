b8feae15c41a9a51f6212d3671d3025402083de4
Csigerkrisz <csigerkrisztian@gmail.com>
2025-05-28 23:20:50 +0200

    Gombok metódusai

--- Film.cs
+++ Film.cs
@@ -1,11 +1,11 @@
 ﻿namespace Wpf_dolgozat1 {
     internal class Film {
-        public string Filmazon { get; private set; }
-        public string Cim { get; private set; }
-        public int Ev { get; private set; }
-        public string Szines { get; private set; }
-        public string Mufaj { get; private set; }
-        public int Hossz { get; private set; }
+        public string Filmazon { get; set; }
+        public string Cim { get; set; }
+        public int Ev { get; set; }
+        public string Szines { get; set; }
+        public string Mufaj { get; set; }
+        public int Hossz { get; set; }
 
         public Film(string filmazon, string cim, int ev, string szines, string mufaj, int hossz) {
             Filmazon = filmazon;
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -25,5 +25,41 @@ namespace Wpf_dolgozat1
             InitializeComponent();
 
         }
+
+        private void lekerdez(object sender, RoutedEventArgs e) {
+            kapcs.Open();
+            var lekerdezes = new MySqlCommand("SELECT * FROM csigeri_filmek", kapcs).ExecuteReader();
+            while (lekerdezes.Read()) { 
+                filmek.Add(new Film(lekerdezes.GetString(0), lekerdezes.GetString(1), lekerdezes.GetInt32(2), lekerdezes.GetString(3), lekerdezes.GetString(4),  lekerdezes.GetInt32(5)));
+            }
+
+            dgAdatok.ItemsSource = filmek;
+            kapcs.Close();
+        }
+
+        private void kivalasztott(object sender, SelectionChangedEventArgs e) {
+            var item = dgAdatok.SelectedItem as Film;
+            Film? film = item;
+            lbFilmAzon.Content = film.Filmazon.ToString();
+            tb1.Text = film.Cim;
+            tb2.Text = film.Ev.ToString();
+            tb3.Text = film.Szines;
+            tb4.Text = film.Mufaj;
+            tb5.Text = film.Hossz.ToString();
+        }
+
+        private void modosit(object sender, RoutedEventArgs e) {
+            kapcs.Open();
+            new MySqlCommand($"UPDATE csigeri_filmek SET cim = '{tb1.Text}', ev = {tb2.Text}, szines = '{tb3.Text}', mufaj = '{tb4.Text}', hossz = {tb5.Text} WHERE filmazon = '{lbFilmAzon.Content}'", kapcs).ExecuteNonQuery();
+            filmek.Clear();
+            var lekerdezes = new MySqlCommand("SELECT * FROM csigeri_filmek", kapcs).ExecuteReader();
+            while (lekerdezes.Read()) {
+                filmek.Add(new Film(lekerdezes.GetString(0), lekerdezes.GetString(1), lekerdezes.GetInt32(2), lekerdezes.GetString(3), lekerdezes.GetString(4), lekerdezes.GetInt32(5)));
+            }
+            dgAdatok.ItemsSource = filmek;
+            dgAdatok.Items.Refresh();
+            kapcs.Close();
+
+        }
     }
 }
